4a5eb32676abd792e24fe16817b49e3152c3a919
Anna <ahannke@gmail.com>
2016-07-28 13:57:12 +0200

    conditionals

---
+++ src/main/java/it/morfoza/java101/Conditionals.java
@@ -0,0 +1,31 @@
+package it.morfoza.java101;
+
+public class Conditionals {
+
+    public static void main(String[] args) {
+
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean result = 1 < 2;
+
+
+        if (result=true) {
+            System.out.println("b1 is true");
+        } else {
+            System.out.println("b1 is false");
+        }
+        if (b2) {
+            System.out.println("b2 is true");
+        } else {
+            System.out.println("b2 is false");
+
+        }
+
+        if (b2==true) {
+            System.out.println("b2 is true");
+        } else {
+            System.out.println("b2 is false");
+
+        }
+    }
+}
